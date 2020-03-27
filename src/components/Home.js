63343e70fd360798a43166d98e014cd23df7cee9
nataliastewart <nataliastewart@users.noreply.github.com>
2020-03-27 12:25:06 -0400

    FormSchema on Pizza.js Done

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,13 +7,8 @@ const Home = () => {
     <div>
       <h1>PIZZA</h1>
       <h2>Build your own pizza and choose your toppings</h2>
-      <img
-        src="C:\Users\natal\Documents\Assignments\Sprint-Challenge-Lambda-Eats-starter\imagePizza\Pizza-2.jpg"
-        alt="pizzapic"
-      />
-      <h3>Click here to build your pizza! 👇</h3>
       <Link className="flexin" to={"/pizza"}>
-        <div className="order-here">PIZZA TIME!</div>
+        <button>BUILD YOUR PIZZA - Click here!</button>
       </Link>
     </div>
   );
--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,5 +1,17 @@
 import React from "react";
+import axios from "axios";
+import * as yup from "yup";
+import { Link } from "react-router-dom";
+
+const formSchema = yup.object().shape({
+  name: yup
+    .string()
+    .required("Please input a name")
+    .min(2, "name must be more than 2 characters"),
+  size: yup.string().required("must choose a pizza size"),
+  topppings: yup.boolean().oneOf([true], "must choose your toppings")
+});
 
 export default function Pizza() {
-  return <div></div>;
+  return <div>PIIIZZA</div>;
 }
